a17f9ef323f89361b4280e18277216a3b4bb71d2
bgpr <prash.bg@gmail.com>
2026-02-25 00:43:43 +1100

    fix: Revert run_pipeline.sh to use proot-distro environment

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,10 +3,11 @@
 
 # Construct the absolute path to the main_processor.py script
 SCRIPT_PATH="/data/data/com.termux/files/home/apk/main_processor.py"
+PROJECT_DIR="/data/data/com.termux/files/home/apk"
 
 # Construct the command to run Python script inside proot-distro
-# Pass all arguments received by run_pipeline.sh directly to main_processor.py
-PROOT_CMD="/opt/venv/bin/python ${SCRIPT_PATH} $@"
+# Added 'cd' to ensure we are in the correct directory for relative paths and logs
+PROOT_CMD="cd ${PROJECT_DIR} && /opt/venv/bin/python ${SCRIPT_PATH} $@"
 
 # Execute proot-distro login with the Python command
 proot-distro login debian --shared-tmp -- env TERM=xterm-256color HOME=/root bash -c "${PROOT_CMD}"
